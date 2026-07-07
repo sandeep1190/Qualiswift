b1c15b0369a38cf2adfac8a9003dd540f41d9379
Sandeep Dhiman <sandeep.dhiman@exate.com>
2026-07-07 15:38:04 +0530

    add favicon

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./index.css";
+import favicon from "./assets/favicon.png";
 
-createRoot(document.getElementById('root')).render(
+// Set favicon
+const link =
+  document.querySelector("link[rel*='icon']") ||
+  document.createElement("link");
+
+link.type = "image/png";
+link.rel = "icon";
+link.href = favicon;
+
+document.head.appendChild(link);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
